2c7301d7a0c3127b35468a1487ae94369d8a9d2d
mau-guerrero <smg10@rice.edu>
2018-10-27 12:11:45 -0500

    adds pins header

---
+++ src/pins.h
@@ -0,0 +1,14 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define STATUS 32
+#define PWM_OUT 30
+#define FAULT_IN 24
+#define FAULT_OUT 7
+#define OP_IN 14
+#define GPIO1 3
+#define GPIO2 2
+#define GPIO3 1
+#define GPIO4 0
+
+#endif // PINS_H
