edc1aa563788d54d43b4e63dc1192f362eae7807
mau-guerrero <smg10@rice.edu>
2018-10-27 14:47:06 -0500

    doc in pins

--- src/pins.h
+++ src/pins.h
@@ -1,12 +1,17 @@
 #ifndef PINS_H
 #define PINS_H
 
+// LED teensy status
 #define STATUS 32
 #define PWM_OUT 30
+// active low input from gatedriver showing status
 #define FAULT_IN 24
+// LED to represent gatedriver underVoltage/overCurrent status
 #define FAULT_OUT 7
+// A0 - used for current sense opamp input
 #define OP_IN 14
-#define PEDAL_IN 3
+// A1 - used for pedal position input
+#define PEDAL_IN 15
 #define GPIO2 2
 #define GPIO3 1
 #define GPIO4 0
