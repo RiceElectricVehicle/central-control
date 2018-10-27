e0524a7d28c85926f175647d777bc57ff81a63b2
mau-guerrero <smg10@rice.edu>
2018-10-27 13:11:45 -0500

    adds pwm code at 14kHz and 12bit reso.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+#include <Arduino.h>
 #include <pins.h>
 
 void setup() {
+  // set PWM frequency, (not 10kHz, 14k is chosen due to H/W)
+  analogWriteFrequency(PWM_OUT, 14648.437);
+  // analogWrite takes values from 0-4095, 4096 for HIGH
+  analogWriteResolution(PWM_OUT);
 }
 
 void loop() {
+  for (int i = 0; i < 4096; i++) {
+    analogWrite(PWM_OUT, i);
+    delay(100);
+  }
 }
