f21c614bc50e245f1d2dd0d942bacce144e09722
mau-guerrero <smg10@rice.edu>
2018-11-06 19:39:57 -0600

    sets up main for testing LED, and PWM out. \n Also adds a little logging library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Logger.h>
 #include <Motor.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
@@ -6,10 +7,12 @@
 
 #define PWM_FREQ 14648.437
 
-Motor motor1(PWM_OUT1, CURRENT_IN1, VOLTAGE_IN1);
-Motor motor2(PWM_OUT2, CURRENT_IN2, VOLTAGE_IN2);
+// Motor motor1(PWM_OUT1, CURRENT_IN1, VOLTAGE_IN1);
+// Motor motor2(PWM_OUT2, CURRENT_IN2, VOLTAGE_IN2);
 volatile bool brk;
 double pedal_power;
+
+Logger logger("REV MC2018", "info");
 
 void setup() {
   // set PWM frequency, (not 10kHz, 14k is chosen due to H/W)
@@ -17,15 +20,29 @@ void setup() {
   // analogWrite takes values from 0-4095, 4096 for HIGH
   analogWriteResolution(12);
   brk == false;
+  logger.init(115200);
+  pinMode(STATUS, OUTPUT);
+  logger.logg("Setup Done");
 }
 
 void loop() {
   // Get power setting from pedal
-  pedal_power = analogRead(PEDAL_IN);
-  motor1.set_power(pedal_power);
-  motor2.set_power(pedal_power);
-  if (brk == true) {
-    motor1.set_power(0);
-    motor2.set_power(0);
+  logger.logi("Blinking satus LED");
+  // blink LED
+  digitalWrite(STATUS, HIGH);
+  delay(200);
+  digitalWrite(STATUS, LOW);
+  delay(200);
+  logger.logi("PWM Frequency test (5 seconds)");
+  analogWrite(PWM_OUT1, 1000);
+  delay(5000);
+  logger.logi("Going over dutycycles:");
+  char str[80];
+  // go over ducty cyle.
+  for (int i = 0; i < 4096; i++) {
+    sprintf(str, "DC: %d", i);
+    logger.logi(str);
+    analogWrite(PWM_OUT1, i);
+    delay(25);
   }
 }
