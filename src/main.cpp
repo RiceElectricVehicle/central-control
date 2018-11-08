ff66235b37c846c4e5e82501d60d1e9a4c19f8f0
mau-guerrero <smg10@rice.edu>
2018-11-08 15:59:40 -0600

    Adds code to catch faults from gate driver. This should be integrated into motor class.

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,52 @@
 
 // Motor motor1(PWM_OUT1, CURRENT_IN1, VOLTAGE_IN1);
 // Motor motor2(PWM_OUT2, CURRENT_IN2, VOLTAGE_IN2);
-volatile bool brk;
+volatile bool fault = false;
 double pedal_power;
 
 Logger logger("REV MC2018", "info");
+
+void fault_catch() {
+  if (fault) {
+    fault = false;
+  } else {
+    fault = true;
+  }
+}
 
 void setup() {
   // set PWM frequency, (not 10kHz, 14k is chosen due to H/W)
   analogWriteFrequency(PWM_OUT1, PWM_FREQ);
   // analogWrite takes values from 0-4095, 4096 for HIGH
   analogWriteResolution(12);
-  brk == false;
+
   logger.init(115200);
+
   pinMode(STATUS, OUTPUT);
+  pinMode(FAULT_OUT, OUTPUT);
+
   logger.logg("Setup Done");
+
   delay(5000);
+
   // blink LED
   logger.logi("Blinking satus LED");
   digitalWrite(STATUS, HIGH);
   delay(200);
   digitalWrite(STATUS, LOW);
   delay(200);
+
+  // disable interrupts
+  cli();
+  attachInterrupt(FAULT_IN, fault_catch, CHANGE);
+  sei();
 }
 
 void loop() {
   pedal_power = 4 * analogRead(PEDAL_IN);
   analogWrite(PWM_OUT1, pedal_power);
+
+  if (!fault) {
+    digitalWrite(FAULT_OUT, HIGH);
+  }
 }
