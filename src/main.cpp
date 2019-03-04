81f23189a2027aff85e367314582a1cac9069e9e
smaug-g <smg10@rice.edu>
2019-03-04 15:42:45 -0600

    lay out ISRs and interval timers

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,24 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <pins.h>
+#include <IntervalTimer.h>
 
 #define PWM_FREQ 14648.437
 
+IntervalTimer pidTimer;
+IntervalTimer getPower;
 Motor motorA(PWM_LA, CURRENT_INA);
 Motor motorB(PWM_LB, CURRENT_INB);
+long time;
+volatile long encAcnt;
+volatile long encBcnt;
 volatile bool brk;
 double pedal_power;
 
 void setup() {
   // set PWM frequency, (not 10kHz, 14k is chosen due to H/W)
   analogWriteFrequency(PWM_LA, PWM_FREQ);
+  analogWriteFrequency(PWM_LB, PWM_FREQ);
   // analogWrite takes values from 0-4095, 4096 for HIGH
   analogWriteResolution(12);
   brk == false;
@@ -22,10 +29,26 @@ void setup() {
 void loop() {
   // Get power setting from pedal
   pedal_power = analogRead(PEDAL_IN);
-  motorA.set_power(pedal_power);
-  motorB.set_power(pedal_power);
   if (brk == true) {
     motorA.set_power(0);
     motorB.set_power(0);
   }
+}
+
+void powerIntervalTimer() {
+  motorA.update_power();
+  motorB.update_power();
+}
+
+void pidIntervalTimer() {
+  motorA.set_power(pedal_power);
+  motorB.set_power(pedal_power);
+}
+
+void encAISR() {
+  encAcnt++;
+}
+
+void encBISR() {
+  encBcnt++;
 }
