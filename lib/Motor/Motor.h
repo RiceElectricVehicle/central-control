31973352d76d3a1058cf0d6e9b7410b84da92097
smaug-g <smg10@rice.edu>
2019-03-05 20:43:35 -0600

    Major overhaul of code to improve traceability.

--- lib/Motor/Motor.h
+++ lib/Motor/Motor.h
@@ -1,30 +1,35 @@
 #include <Arduino.h>
-#include <CurrentSense.h>
 #include <PID_v1.h>
+
 
 #ifndef MOTOR_H
 #define MOTOR_H
-
+#define BAT_V 48
 class Motor {
  private:
   double current;
   double voltage;
-  double measured_power;
+  double pid_input;
   double set_point;
-  double out_power;
+  double pid_output;
   double kp;
   double ki;
   double kd;
   byte motor_output;   // Stores output pin
-  byte motor_current;  // Stores current pin
+  byte motor_current_pin;  // Stores current pin
   PID pid_obj;
-  CurrentSense current_sense;
 
  public:
   Motor(byte output_pin, byte current_pin);
-  void update_power();                // Change mesuredPower
-  void set_power(float pedal_power);  // Change setpoint
-  double get_power();                 // Get current power setting
+  void update_pid_input();                // Change mesuredPower
+  void compute_pid(double pedal_power);  // Change setpoint
+  double get_pid_output();                 // Get current power setting
+  void set_pwm();
+  void set_zero();
+  double get_current();
+  double get_voltage(double current_duty_cycle);
+  int get_duty_cycle();
+  void start();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <pins.h>
-#include <IntervalTimer.h>
 
 #define PWM_FREQ 14648.437
 
@@ -11,44 +10,83 @@ IntervalTimer pidTimer;
 IntervalTimer getPower;
 Motor motorA(PWM_LA, CURRENT_INA);
 Motor motorB(PWM_LB, CURRENT_INB);
-long time;
-volatile long encAcnt;
-volatile long encBcnt;
+volatile long revolutions;
 volatile bool brk;
 double pedal_power;
+double rpm;
 
 void setup() {
+  pinMode(PWM_LA, OUTPUT);
+  pinMode(PWM_LB, OUTPUT);
+  digitalWrite(PWM_LA, HIGH);
+  digitalWrite(PWM_LB, HIGH);
   // set PWM frequency, (not 10kHz, 14k is chosen due to H/W)
   analogWriteFrequency(PWM_LA, PWM_FREQ);
   analogWriteFrequency(PWM_LB, PWM_FREQ);
   // analogWrite takes values from 0-4095, 4096 for HIGH
   analogWriteResolution(12);
-  brk == false;
+
+  pinMode(STATUS, OUTPUT);
+  pinMode(OCP_OUTA, OUTPUT);
+  pinMode(OCP_OUTB, OUTPUT);
+  pinMode(OCP_INA, INPUT);
+  pinMode(OCP_INB, INPUT);
+
+  // blink LED
+  digitalWrite(STATUS, HIGH);
+  digitalWrite(OCP_OUTA, HIGH);
+  digitalWrite(OCP_OUTB, HIGH);
+  delay(200);
+  digitalWrite(STATUS, LOW);
+  digitalWrite(OCP_OUTA, LOW);
+  digitalWrite(OCP_OUTB, LOW);
+  delay(200);
+
+  // disable interrupts
+  cli();
+  // attachInterrupt(FAULT_IN, fault_catch, CHANGE);
+  attachInterrupt(BRAKE, brake_isr, CHANGE);
+  attachInterrupt(ENCXZ, enc_isr, RISING);
+  // enable interrups
+  sei();
+  motorA.start();
+  motorB.start();
 }
 
 void loop() {
   // Get power setting from pedal
-  pedal_power = analogRead(PEDAL_IN);
+  pedal_power = map(analogRead(PEDAL_IN), 550, 900, 0, 4096);
   if (brk == true) {
-    motorA.set_power(0);
-    motorB.set_power(0);
+    motorA.set_zero();
+    motorB.set_zero();
+  } else {
+    motorA.update_pid_input();
+    motorB.update_pid_input();
+    // Run PID computations.
+    motorA.compute_pid(pedal_power);
+    motorB.compute_pid(pedal_power);
+    // Update PWM outputs.
+    motorA.set_pwm();
+    motorB.set_pwm();
   }
+  rpm = get_rpm(millis());
+
 }
 
-void powerIntervalTimer() {
-  motorA.update_power();
-  motorB.update_power();
+void brake_isr() {
+  if (brk)
+    brk = false;
+  else
+    brk = true;
 }
 
-void pidIntervalTimer() {
-  motorA.set_power(pedal_power);
-  motorB.set_power(pedal_power);
+void enc_isr() {
+  revolutions++;
 }
 
-void encAISR() {
-  encAcnt++;
-}
-
-void encBISR() {
-  encBcnt++;
+double get_rpm(uint32_t time_now) {
+  static uint32_t time_old = 0;
+  double rpm_new = revolutions / (60 * (time_now - time_old));
+  time_old = millis();
+  return rpm_new;
 }
