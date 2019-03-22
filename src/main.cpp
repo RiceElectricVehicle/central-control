a2e0287d3fc1975c77a8c51e2d3eca1ec52648f3
smaug-g <smg10@rice.edu>
2019-03-22 02:31:09 -0500

    best testing code

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 // Motor motor2(PWM_OUT2, CURRENT_IN2, VOLTAGE_IN2);
 volatile bool fault = false;
 double pedal_power;
+int fan_speed;
+int pedal_adc;
 
 Logger logger("REV MC2018", "info");
 
@@ -32,6 +34,8 @@ void setup() {
   // set PWM frequency, (not 10kHz, 14k is chosen due to H/W)
   analogWriteFrequency(PWM_LA, PWM_FREQ);
   analogWriteFrequency(PWM_LB, PWM_FREQ);
+  analogWriteFrequency(FAN1, PWM_FREQ);
+  analogWriteFrequency(FAN2, PWM_FREQ);
   // analogWrite takes values from 0-4095, 4096 for HIGH
   analogWriteResolution(12);
 
@@ -42,6 +46,8 @@ void setup() {
   pinMode(OCP_OUTB, OUTPUT);
   pinMode(OCP_INA, INPUT);
   pinMode(OCP_INB, INPUT);
+  pinMode(FAN1, OUTPUT);
+  pinMode(FAN2, OUTPUT);
 
   logger.logg("Setup Done");
 
@@ -63,7 +69,8 @@ void setup() {
   // sei();
 }
 
-int current_read;
+int current_readA;
+int current_readB;
 char buffer[50];
 void loop() {
   // pedal_power = 4 * analogRead(PEDAL_IN);
@@ -78,20 +85,33 @@ void loop() {
   //   delay(1);
   // }
 
-  pedal_power = map(analogRead(PEDAL_IN), 550, 900, 4095, 0);
+  pedal_adc = analogRead(PEDAL_IN);
+  pedal_power = map(pedal_adc, 550, 900, 4095, 0);
+  fan_speed = map(pedal_adc, 550, 900, 700, 1000);
   // sprintf(buffer, "Pedal reading: %f", pedal_power);
   // logger.logg(buffer);
 
   analogWrite(PWM_LA, pedal_power);
   analogWrite(PWM_LB, pedal_power);
-  current_read = analogRead(CURRENT_INA);
+  if (pedal_adc < 650) {
+    fan_speed = 0;
+  } else {
+    fan_speed = map(pedal_adc, 550, 900, 700, 2000);
+  }
+  analogWrite(FAN1, fan_speed);
+  analogWrite(FAN2, fan_speed);
+  current_readA = analogRead(CURRENT_INA);
+  current_readB = analogRead(CURRENT_INB);
+  // delay(50);
   // Serial.print(1050);
   // Serial.print("\t");
-  Serial.print(current_read);
+  // Serial.println(current_readA);
+  // Serial.print("\t");
+  // Serial.println(current_readB);
   // Serial.print("\t");
   // Serial.println(-5);
-  sprintf(buffer, "Current reading: %u", current_read);
-  logger.logg(buffer);
+  // sprintf(buffer, "Current reading: %u", current_read);
+  // logger.logg(buffer);
   // digitalWrite(OCP_OUTA, digitalRead(OCP_INA));
   // digitalWrite(OCP_OUTB, digitalRead(OCP_INB));
 }
